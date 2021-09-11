62116b403f6f13ad639fca651ece73c6718accd2
JIAJUN CHEN <startbin90@gmail.com>
2021-09-11 21:41:38 +0000

    make message pretty

--- main.py
+++ main.py
@@ -6,27 +6,42 @@ from discord.ext import tasks
 from keep_alive import keep_alive
 from datetime import datetime
 
+# Json format
+# {
+#   name:
+#     { "shares": 0,
+#       "latest_time": start_ts,
+#     },
+# }
+
+
 def str_to_ts(s):
     return datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%fZ")
 
+
 def ts_to_str(ts):
     return ts.strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
 
 log_channel = 885966677689401386
 test_channel = 639512541701079073
 test_nano_wallet = "0x7c3d72a477f3d36a34c68990a9e62a48c0331710"
 test_spark_wallet = "sp_ethereum"
+log_spark_wallet = "sp_startbin"
 nanopool_api_addr = "https://api.nanopool.org/v1/eth/workers/"
 sparkpool_api_addr = "https://www.sparkpool.com/v1/worker/"
 start_ts = ts_to_str(datetime(2021, 9, 1))
-channel_id = test_channel
-eth_wallet = test_spark_wallet
+channel_id = log_channel
+eth_wallet = log_spark_wallet
 pool_api_addr = sparkpool_api_addr
+update_to_channel = True 
 
 
 class worker_dict:
     def __init__(self, d=dict()):
         self.d = d
+        self.loaded_from_json = False
+        self.online_workers = []
 
     def get(self):
         return self.d
@@ -40,12 +55,6 @@ class worker_dict:
             return self.d[name]["latest_time"]
 
     def set(self, name, shares=0, ts=start_ts):
-        # {
-        #   name:
-        #     { "shares": 0,
-        #       "latest_time": start_ts,
-        #     },
-        # }
         self.d[name] = {"shares": shares, "latest_time": ts}
         self.dump_to_file()
 
@@ -55,50 +64,81 @@ class worker_dict:
             self.d[name]["latest_time"] = ts
             self.dump_to_file()
 
+    def set_online_workers(self, lst):
+        self.online_workers = lst
+
     def load_from_json(self):
         try:
             with open("local_log.json", "r") as f:
                 s = f.read()
                 self.d = json.loads(s)
+                self.loaded_from_json = True
         except:
             self.d = {}
+            self.loaded_from_json = False
 
     def dump_to_file(self):
         try:
             with open("local_log.json", "w") as f:
                 json.dump(self.d, f)
-        except Exception as e: 
+        except Exception as e:
             print(e)
             print("dump failed")
 
     def __str__(self):
-        return str(self.d)
+        msg = ""
+        for key, value in self.d.items():
+            if key in self.online_workers:
+                msg += ":green_circle:  "
+            else:
+                msg += ":red_circle:  "
+            msg += key + " has "
+            msg += str(value["shares"])
+            msg += " shares. Last report time: "
+            msg += str(value["latest_time"]) + "\n\n"
+        msg += ":green_circle:  The worker is registered on SparkPool  "
+        msg += ":red_circle:  The worker is removed from SparkPool for 24h inactivity"
+        return msg
 
 
 workers = worker_dict()
-workers.load_from_json()
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    msg = "\n:white_check_mark: Bot is online. "
+    workers.load_from_json()
+    fetch_workers()
+    if workers.loaded_from_json:
+        msg += "I recovered these workers from logfile:\n\n"
+        msg += str(workers) + "\n\n"
+    else:
+        msg += "I didn't found anything from logfile\n\n"
+    msg += "Available commands:\n"
+    msg += "$list     List all workers stat\n"
+    msg += "$v        Verbose, update share increment to the channel\n"
+    msg += "$shutup   Disable share update\n"
+    await client.get_channel(channel_id).send(msg)
+    
     fetch_data.start()
 
 
 @client.event
 async def on_message(message):
     if message.content.startswith("$list"):
-        msg = ""
-        for key, value in workers.get().items():
-            msg += key + " has "
-            msg += str(value["shares"])
-            msg += " shares.\n"
+        msg = str(workers)
         await client.get_channel(channel_id).send(msg)
+    if message.content.startswith("$v"):
+        update_to_channel = True
+        await client.get_channel(channel_id).send("Verbose mode ON")
+    if message.content.startswith("$shutup"):
+        update_to_channel = False
+        await client.get_channel(channel_id).send(
+            "Verbose mode OFF, Now I shutup :slight_smile: ")
 
-
-@tasks.loop(seconds=5)
-async def fetch_data():
-    print(workers.get())
+def fetch_workers():
     payload1 = {'currency': 'ETH', 'miner': eth_wallet}
     r1 = requests.get(pool_api_addr + "list", params=payload1)
     if r1.status_code != 200:
@@ -106,10 +146,13 @@ async def fetch_data():
     res1 = json.loads(r1.text)
     if res1["code"] != 200:
         return
-    for worker in res1["data"]:
+    online_workers = [ worker["worker"] for worker in res1["data"]]
+    workers.set_online_workers(online_workers)
 
-        name = worker["worker"]
-
+@tasks.loop(seconds=5)
+async def fetch_data():
+    fetch_workers()
+    for name in workers.online_workers:
         if name not in workers.get():
             await client.get_channel(channel_id).send("welcome new worker: " +
                                                       name)
@@ -122,20 +165,18 @@ async def fetch_data():
         res2 = json.loads(r2.text)
         if res2["code"] != 200:
             return
-        # 2021-09-10T04:40:00.000Z
-        # res2["data"].sort(key=lambda x: str_to_ts(x["time"]))
         shares_delta = 0
         latest_time = start_ts
         has_change = False
         for entry in res2["data"]:
-            # print(str_to_ts(entry["time"]))
-            # print(workers.get_latest_time(name))
-            if str_to_ts(entry["time"]) > str_to_ts(workers.get_latest_time(name)):
+            if str_to_ts(entry["time"]) > str_to_ts(
+                    workers.get_latest_time(name)):
                 has_change = True
                 shares_delta += entry["validShares"]
-                latest_time = entry["time"] if str_to_ts(entry["time"]) > str_to_ts(latest_time) else latest_time
+                latest_time = entry["time"] if str_to_ts(
+                    entry["time"]) > str_to_ts(latest_time) else latest_time
         if has_change:
-            if shares_delta:
+            if shares_delta and update_to_channel:
                 await client.get_channel(channel_id).send(
                     name + "'s share: " + str(workers.get_shares(name)) +
                     " -> " + str(workers.get_shares(name) + shares_delta))
